ddbae797ac98c4026110c04aae4147fe600a64ed
mkbaker <mattbakerk@gmail.com>
2016-12-21 08:22:13 -0600

    Initial commit.

---
+++ DoThis/Messages.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinTail
+{
+    class Messages
+    {
+        #region Neutral/system messages
+        /// <summary>
+        /// Marker class to continue processing.
+        /// </summary>
+        public class ContinueProcessing { }
+        #endregion
+
+        #region Success messages
+        // in Messages.cs
+        /// <summary>
+        /// Base class for signalling that user input was valid.
+        /// </summary>
+        public class InputSuccess
+        {
+            public InputSuccess(string reason)
+            {
+                Reason = reason;
+            }
+
+            public string Reason { get; private set; }
+        }
+        #endregion
+
+        #region Error messages
+        #endregion
+    }
+}

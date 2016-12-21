689c0d4528def67355320477d65ad52d27636f00
mkbaker <mattbakerk@gmail.com>
2016-12-21 11:01:05 -0600

    Fixed the 'consoleWriterProps' call

--- DoThis/Program.cs
+++ DoThis/Program.cs
@@ -13,7 +13,7 @@ namespace WinTail
             // initialize MyActorSystem
             MyActorSystem = ActorSystem.Create("MyActorSystem");
 
-            Props consoleWriterProps = Props.Create(typeof(ConsoleWriterActor));
+            Props consoleWriterProps = Props.Create<ConsoleWriterActor>();
 
             IActorRef consoleWriterActor = MyActorSystem.ActorOf(consoleWriterProps, "consoleWriterActor");
 
